5e41fbe90f287a45625bef190400f61361af2bd9
Zanele01 <0706885n@students.wits.ac.za>
2019-07-05 14:18:05 +0200

    Code for tweet extraction

---
+++ FileReader.py
@@ -0,0 +1,15 @@
+import json
+
+tweets_data_path = 'twitter_data.txt'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+
+print (len(tweets_data))
+print (tweets_data[0]['text'])
