e9f60194cf6580b8fae3edf3a501dc83d5393db1
kdzumba <knowledgedzumba@gmail.com>
2019-07-08 09:56:49 +0200

    added funtionality for removing twitter usernames

--- FileReader.py
+++ FileReader.py
@@ -1,8 +1,17 @@
 import json
+import re
+
+#Removes all twitter usernames from the tweet text (using the regurlar expression in regx)
+def remove_usernames(tweet_text):
+	regex = r'@\w*\s'
+	tweet_text = re.sub(regex, '', tweet_text)
+
 
 tweets_data_path = 'twitter_data.txt'
 
 tweets_data = []
+
+#Creating a file object for reading the tweets data 
 tweets_file = open(tweets_data_path, "r")
 for line in tweets_file:
     try:
@@ -11,5 +20,18 @@ for line in tweets_file:
     except:
         continue
 
+#Printing the number of tweets that have been loaded into the tweets file
 print (len(tweets_data))
-print (tweets_data[0]['text'])
+
+#Extracting a tweet text from tweets data
+for tweet in tweets_data:
+	if 'retweeted_status' in tweet:
+		if 'extended_tweet' in tweet['retweeted_status']:
+			text = tweet['retweeted_status']['extended_tweet']['full_text']
+			remove_usernames(text)
+			print(text)
+	else:
+		remove_usernames(tweet['text'])
+		print(text)
+
+
