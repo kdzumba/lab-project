f43b5e6ba5a44d471e42737dcd4d7fda8092cb35
kdzumba <knowledgedzumba@gmail.com>
2019-07-24 17:58:45 +0200

    commented out data extraction and data conversion

--- FileReader.py
+++ FileReader.py
@@ -6,17 +6,17 @@ data/dataset.txt and the overall dataset is stored in data/dataset.csv
 """
 import Preprocessing as processor
 
-raw_data_file = "data/twitter_data.txt"
-processed_data_file = "data/dataset.txt"
+#raw_data_file = "data/twitter_data.txt"
+#processed_data_file = "data/dataset.txt"
 dataset_file = "data/dataset.csv"
 
 if __name__ == "__main__":
 
     # Get unlabelled dataset from raw tweets data
-    processor.get_tweets(raw_data_file, processed_data_file)
+    #processor.get_tweets(raw_data_file, processed_data_file)
 
     # Get dataset in csv format from textfile data
-    processor.convert_to_csv(processed_data_file, dataset_file)
+    #processor.convert_to_csv(processed_data_file, dataset_file)
 
     # Remove duplicate tweets from the dataset
     processor.remove_duplicates(dataset_file)
