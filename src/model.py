fffe70d9654af60168fa1dc1b3fdea26e68f4db4
kdzumba <knowledgedzumba@gmail.com>
2019-08-17 01:25:23 +0200

    modified get_model() function so that it takes a classifier as argument

--- src/model.py
+++ src/model.py
@@ -1,5 +1,3 @@
-from sklearn import svm
-from sklearn.svm import SVC
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -16,45 +14,49 @@ from joblib import dump, load
 import features
 
 
-def get_model():
+def get_model(estimator=None):
 
-    data = pd.read_csv("./data/dataset.csv", encoding="utf-8")
-    tfidf_feature = pd.read_csv("./data/tfidf_feature.csv", encoding="utf-8")
-    dict_feature = pd.read_csv("./data/dictionary.csv", encoding="utf-8")
-    sentiment_feature = pd.read_csv("./data/sentiment.csv", encoding="utf-8")
+    if estimator is not None:
+        data = pd.read_csv("./data/dataset.csv", encoding="utf-8")
+        tfidf_feature = pd.read_csv("./data/tfidf_feature.csv", encoding="utf-8")
+        dict_feature = pd.read_csv("./data/dictionary.csv", encoding="utf-8")
+        sentiment_feature = pd.read_csv("./data/sentiment.csv", encoding="utf-8")
 
-    df_list = [data, sentiment_feature, dict_feature, dict_feature, tfidf_feature]
+        df_list = [data, sentiment_feature, dict_feature, dict_feature, tfidf_feature]
 
-    master = df_list[0]
+        master = df_list[0]
 
-    for df in df_list[1:]:
-        master = master.merge(df, on="index")
+        for df in df_list[1:]:
+            master = master.merge(df, on="index")
 
-    y = master.iloc[:, 1]
-    X = master.iloc[:, 3:]
+        y = master.iloc[:, 1]
+        X = master.iloc[:, 3:]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.4, random_state=42
-    )
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.4, random_state=42
+        )
 
-    scaler = MinMaxScaler()
-    scaler.fit(X_train)
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+        scaler = MinMaxScaler()
+        scaler.fit(X_train)
+        X_train_scaled = scaler.transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
 
-    comp_NB = ComplementNB()
-    comp_NB.fit(X_train_scaled, y_train)
-    comp_pred = comp_NB.predict(X_test_scaled)
+        # comp_NB = ComplementNB()
+        estimator.fit(X_train_scaled, y_train)
+        predictions = estimator.predict(X_test_scaled)
 
-    print("Complement Naive Bayes")
-    print("Accuracy Score: ", accuracy_score(y_test, comp_pred))
-    print("Confusion Matrix: \n", confusion_matrix(y_test, comp_pred))
+        print("Accuracy Score: ", accuracy_score(y_test, predictions))
+        print("Confusion Matrix: \n", confusion_matrix(y_test, predictions))
 
-    return comp_NB
+    return estimator
+
 
 if __name__ == "__main__":
 
-    model = get_model()
+    classifier_1 = ComplementNB()
+    classifier_2 = LogisticRegression(solver="lbfgs")
+
+    model = get_model(classifier_2)
 
     tweet = input("Tweet: ")
 
@@ -66,7 +68,9 @@ if __name__ == "__main__":
             dic_feature2 = pd.DataFrame(features.term_frequency(tweet), index=[0])
             tfidf_feature = features.get_tfidf_scores(tweet)
 
-            tweet_df = sen_feature.merge(dic_feature1, left_index=True, right_index=True)
+            tweet_df = sen_feature.merge(
+                dic_feature1, left_index=True, right_index=True
+            )
             tweet_df = tweet_df.merge(dic_feature2, left_index=True, right_index=True)
             tweet_df = tweet_df.merge(tfidf_feature, left_index=True, right_index=True)
 
